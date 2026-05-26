65c98f9fe4f8ba8ee8136abd800a5e8af44bea86
Marta <martace@student.agh.edu.pl>
2026-05-26 20:46:35 +0200

    Change colorbar and styling

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ class HabitFlow extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: const Color.fromARGB(255, 184, 239, 64)),
+        colorScheme: .fromSeed(seedColor: const Color.fromARGB(255, 34, 192, 28)),
       ),
       home: const MyHomePage(title: 'HabitFlow Home Page'),
     );
@@ -77,45 +77,26 @@ class _MyHomePageState extends State<MyHomePage> {
     // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        title: const Text('HabitFlow',style: TextStyle(fontWeight: FontWeight.w600,color: Colors.white,),),
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: .center,
+       child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text('HabitFlow progress counter'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            const Text('Welcome to HabitFlow',style: TextStyle(fontSize: 20),),
+            const SizedBox(height: 10),
+            Text('HabitFlow progress counter: $_counter',style: Theme.of(context).textTheme.headlineMedium,),
+            const SizedBox(height: 20),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Habit progress',
-        child: const Icon(Icons.add),
+        child: const Icon(Icons.add_circle_outline),
       ),
     );
   }
