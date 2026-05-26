04419b2cddac481eecdf660af48968399b6d8333
Marta <martace@student.agh.edu.pl>
2026-05-26 19:53:54 +0200

    Change the name on the application's main page

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const HabitFlow());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class HabitFlow extends StatelessWidget {
+  const HabitFlow({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'HabitFlow',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -28,9 +28,9 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: .fromSeed(seedColor: const Color.fromARGB(255, 184, 239, 64)),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'HabitFlow Home Page'),
     );
   }
 }
